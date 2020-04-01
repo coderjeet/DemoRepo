6f89e0b540fea51e9d1ada9b08b10ab657d2fed5
Kutuli <developer.prasanjeet@gmail.com>
2020-04-01 13:08:12 +0530

    Fisrt git commit

---
+++ src/main/java/com/qa/base/TestBase.java
@@ -0,0 +1,8 @@
+package com.qa.base;
+
+public class TestBase {
+	
+	public void testBaseClass() {
+		System.out.println("Test class");
+	}
+}
---
+++ src/test/java/demo/Demo.java
@@ -0,0 +1,12 @@
+package demo;
+
+import org.testng.annotations.Test;
+
+public class Demo {
+
+	
+	@Test
+	public void testM() {
+		System.out.println("Test ran successfully");
+	}
+}
