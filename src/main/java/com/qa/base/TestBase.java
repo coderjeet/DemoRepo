78c1cf6c42066367c8df1e51163e4d407a8533f3
coderjeet <developer.prasanjeet@gmail.com>
2020-04-22 23:45:38 +0530

    Added new method

--- src/main/java/com/qa/base/TestBase.java
+++ src/main/java/com/qa/base/TestBase.java
@@ -10,4 +10,13 @@ public class TestBase {
 		System.out.println("Test class by git");
  
 	}
+
+	public void testBaseClass() {
+		System.out.println("Test class");
+
+		System.out.println("New print statement");
+
+		System.out.println("Test class by git");
+ 
+	}
 }
